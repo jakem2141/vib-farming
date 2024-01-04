ed34c875f5aeecf68aebefd6efd85af42a130aac
Jake Murray <j1993murray@hotmail.com>
2024-01-04 21:47:12 +0000

    Config restructure + second animal spawn

--- config.lua
+++ config.lua
@@ -6,22 +6,17 @@ Config.Market = {
 
 }
 
-Config.Pigs = {
-    Model = "a_c_pig",
-    TimeInMinutesBetweenHarvest = 5,
-    AmountOfPigs = 5,
-    SpawnLocation = vector4(2173.91, 4962.89, 41.36, 174.67)
+Config.Animals = {
+    Pigs = {
+        Model = "a_c_pig",
+        TimeInMinutesBetweenHarvest = 5,
+        AmountOfAnimals = 5,
+        SpawnLocation = vector4(2173.91, 4962.89, 41.36, 174.67)
+    },
+    Cows = {
+        Model = "a_c_cow",
+        TimeInMinutesBetweenHarvest = 5,
+        AmountOfAnimals = 5,
+        SpawnLocation = vector4(2182.7, 4973.23, 41.34, 287.3)
+    }
 }
-
--- Config.Cows = {
---     Model = "a_c_cow",
---     TimeInMinutesBetweenHarvest = 5,
---     {
---         Location = vector4(),
-
---     },
---     {
---         Location = vector4(),
-
---     },
--- }
--- server/animals.lua
+++ server/animals.lua
@@ -10,25 +10,26 @@ cows = {}
 ---------
 
 CreateThread(function()
-    local pigInfo = Config.Pigs
+    local pigInfo = Config.Animals.Pigs
+    local cowInfo = Config.Animals.Cows
 
-    for i = 1, Config.Pigs.AmountOfPigs do
+    for i = 1, pigInfo.AmountOfAnimals do
         Wait(500)
-        local pig = CreatePed(1, Config.Pigs.Model, Config.Pigs.SpawnLocation.xyz, true, true)
+        local pig = CreatePed(1, pigInfo.Model, pigInfo.SpawnLocation.xyz, true, true)
         Wait(500)
         local serverEntity = NetworkGetNetworkIdFromEntity(pig)
-        
+
         TriggerClientEvent('farming:client:TargetAnimalEntity', -1, i, serverEntity)
         TriggerClientEvent('farming:client:ControlSpawnedAnimals', -1, i, pigInfo, serverEntity)
     end
+
+    for i = 1, cowInfo.AmountOfAnimals do
+        Wait(500)
+        local cow = CreatePed(1, cowInfo.Model, cowInfo.SpawnLocation.xyz, true, true)
+        Wait(500)
+        local serverEntity = NetworkGetNetworkIdFromEntity(cow)
+        
+        TriggerClientEvent('farming:client:TargetAnimalEntity', -1, i, serverEntity)
+        TriggerClientEvent('farming:client:ControlSpawnedAnimals', -1, i, cowInfo, serverEntity)
+    end
 end)
-
---------
---Cows--
---------
-
--- CreateThread(function()
---     for k, v in pairs(Config.Cows) do
---         -- Do cow spawning stuff here
---     end
--- end)
